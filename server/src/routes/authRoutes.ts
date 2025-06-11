73665bbdee4db158a30905e40c0bc4e452582991
KhushiKumari0203 <Khushi0khushi1khushi2khushi3@gmail.com>
2025-06-11 16:17:04 +0530

    Update authRoutes.ts

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -3,9 +3,7 @@ import { login, register } from '../controllers/authController';
 
 const router = express.Router();
 
-// Routes
 router.post('/login', login);
 router.post('/register', register);
 
-// Default export
 export default router;
