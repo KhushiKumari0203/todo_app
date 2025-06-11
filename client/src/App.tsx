d398df8613b04b508d94b8968a846d88ebd9df1c
KhushiKumari0203 <Khushi0khushi1khushi2khushi3@gmail.com>
2025-06-11 14:55:41 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AddTask from './pages/AddTask';
-import Login from './pages/Login'; // ✅ NEW: import login page
+import Login from './pages/Login'; // 👈 import Login
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />          {/* 👈 Default route: login */}
-        <Route path="/home" element={<Home />} />       {/* 👈 Home after login */}
-        <Route path="/add" element={<AddTask />} />     {/* 👈 Add Task page */}
-        <Route path="/login" element={<Login />} />     {/* 👈 Explicit login route */}
+        <Route path="/" element={<Login />} /> {/* 👈 set as default */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/add" element={<AddTask />} />
       </Routes>
     </BrowserRouter>
   );
